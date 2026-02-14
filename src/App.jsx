d46572c5a6add9a5022aa5e5899d26fa55b64c01
Jaswanth L <sec23cs092@sairamtap.edu.in>
2026-02-14 23:50:28 +0530

    Fix Home page casing for Netlify build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Clark from './pages/Clark'
 import Fortress from './pages/Fortress'
 
